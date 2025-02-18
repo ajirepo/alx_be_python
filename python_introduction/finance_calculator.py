76d6d89c99cddd4138944c2f0b26b010ad2a96d9
Mohammed Abba Aji <ajinote9@gmail.com>
2025-02-18 06:23:22 -0800

    Added finance _calculator file

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,5 @@
+#!/bin/bash
+Monthly_income= int(input("Enter your monthly income:"))
+Monthly_Expense = int(input(“Enter your total monthly expenses:”))
+Monthly_Savings = Monthly_income - Monthly_expense
+Projected_Savings = Monthly_Savings * 12 + (Monthly_Savings * 12 * 0.05))
