4e179bb2151cf3b5003d6447fd6c6893fb4a2463
Mohammed Abba Aji <ajinote9@gmail.com>
2025-02-19 07:33:35 -0800

    Added weather file

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,14 +1,14 @@
 #!/bin/bash
-wheather_today = str(input(print("What's the weather like today? (sunny/rainy/cold):")))
+wheather = str(input(print("What's the weather like today? (sunny/rainy/cold):")))
 
-if wheather = "sunny":
+if wheather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 
-elif wheather = "rainy":
+elif wheather == "rainy":
     print ("Don't forget your umbrella and a raincoat.")
 
-elif wheather = "cold":
+elif wheather == "cold":
     print ("Make sure to wear a warm coat and a scarf")
 
-elif:
+else:
     print("Sorry, I don't have recommendations for this weather.")
